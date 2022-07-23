0052873427693dff40e3f44b61e939f997001101
LAPTOP-UD5SA7D6\LYDIA <shsurya20@gmail.com>
2022-07-23 14:08:55 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { generateUploadURL } from './s3.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.static('front'))
+app.use(cors({
+  origin: '*'
+}))
 
 app.get('/s3Url', async (req, res) => {
   const url = await generateUploadURL()
