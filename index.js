9e2c02c757b256ebd0cad39c60e62a1cc43a7a73
LAPTOP-UD5SA7D6\LYDIA <shsurya20@gmail.com>
2022-09-19 09:31:57 +0530

    added netlify attempt 2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import app from './server.js';
+import { app } from './server.js';
 
 app.listen(3000, () => console.log('%cDebug SERVER :: ', 'background: #222; color: #bada55', 'App running on 3000'));
